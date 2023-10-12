3227de3f147b992299d9a742c2f2ed046d8e6aa2
Jan Meuser <58228946+janmeuser@users.noreply.github.com>
2023-10-12 11:08:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write('Hello world!')
+_max_width_(70)
+st.title("California Housing Prices Prediction")
+st.markdown("""
+##### A web application for predicting California Housing Prices.
+ 
+This app uses machine learning to predict the price of the house. 
+It loads a pre-trained linear regression model, which takes as input various features of the house, 
+such as the number of rooms, the number of bedrooms, the population of the house's neighborhood, and the distance to the nearest city. 
+The app preprocesses the input data by combining some of the features and adding new features, such as the distance to the nearest city.
+""")
